30751b1851bc2e392c8e5255f3e3d4f0a7469a66
mariafjesus <mariafjesus4@gmail.com>
2024-08-12 12:06:49 +0100

    First commit

---
+++ app/_layout.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+const RootLayout = () => {
+    return (
+        <View>
+            <Text> textInComponent </Text>
+        </View>
+    )
+}
+
+export default RootLayout
+
+const styles = StyleSheet.create({})
