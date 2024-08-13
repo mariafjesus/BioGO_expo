0c35b8ccf7f076c8273f78f84857bba9c8ffc1cb
mariafjesus <mariafjesus4@gmail.com>
2024-08-13 17:34:23 +0100

    add icons and images

---
+++ constants/icons.js
@@ -0,0 +1,15 @@
+import camera from "../assets/icons/camera.png";
+import home from "../assets/icons/home.png";
+import map from "../assets/icons/map.png";
+import medal from "../assets/icons/medal.png";
+import eye from "../assets/icons/eye.png";
+import eye_hide from "../assets/icons/eye_hide.png";
+
+export default {
+  camera,
+  home,
+  map,
+  medal,
+  eye,
+  eye_hide,
+};
---
+++ constants/images.js
@@ -0,0 +1,7 @@
+import logo from "../assets/images/logo.png";
+import background_logo from "../assets/images/background_logo.png";
+
+export default {
+  logo,
+  background_logo,
+};
